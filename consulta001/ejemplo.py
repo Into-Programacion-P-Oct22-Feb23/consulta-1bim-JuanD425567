dc512fae43239cef92650ef9420343adb5755aa1
JuanD425567 <115765197+JuanD425567@users.noreply.github.com>
2022-11-28 14:54:08 -0500

    Add files via upload

---
+++ consulta001/ejemplo.py
@@ -0,0 +1,8 @@
+nombre = input ("Escriba su nombre")
+print ("hola {nombre}")
+
+apellido = input ("Ingrese su apellido")
+print ("tu apellido es {apellido}")
+
+edad = input ("Ingrese su edad")
+print ("Tu edad es {edad}")
